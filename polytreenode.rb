85ed52776e992b562695ee2c66c9fa7fd3fe14d2
App Academy Student <student@appacademy.io>
2016-09-16 11:01:02 -0700

    Added #add_child and #remove_child methods

--- polytreenode.rb
+++ polytreenode.rb
@@ -8,23 +8,17 @@ class PolyTreeNode
   end
 
   def parent=(parent)
+    @parent.children.delete(self) if @parent
     @parent = parent
-    parent.children << self
+    parent.children << self if parent
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    child_node.parent = nil
+  end
+
 end
-
-n1 = PolyTreeNode.new("root1")
-n2 = PolyTreeNode.new("root2")
-n3 = PolyTreeNode.new("root3")
-
-# connect n3 to n1
-n3.parent = n1
-# connect n3 to n2
-n3.parent = n2
-
-# this should work
-raise "Bad parent=!" unless n3.parent == n2
-raise "Bad parent=!" unless n2.children == [n3]
-
-# this probably doesn't
-raise "Bad parent=!" unless n1.children == []
